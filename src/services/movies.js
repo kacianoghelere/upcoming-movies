480e2f307640ad931c28ab4b1a4e9e00f0fe04a5
Kaciano Ghelere <themohawkeagle@gmail.com>
2020-01-21 15:47:32 -0300

    request service updated

--- src/services/movies.js
+++ src/services/movies.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const httpClient = axios.create({
   baseURL: `${process.env.REACT_APP_MOVINDEX_API_BASE_URL}`,
   headers: {
+    'Access-Control-Allow-Origin': '*',
     'Authorization': `${process.env.REACT_APP_MOVINDEX_KEY}`
   },
   timeout: 30000
