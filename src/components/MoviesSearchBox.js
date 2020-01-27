7534ce9486253ebca64468b836e674ec25fea0d8
Kaciano Ghelere <themohawkeagle@gmail.com>
2020-01-27 13:42:53 -0300

    Searchbox fixed

--- src/components/MoviesSearchBox.js
+++ src/components/MoviesSearchBox.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import debounce from 'lodash.debounce'
 
 import { fetchMoviesList } from '../store/actions'
 import './MoviesSearchBox.scss'
